4ed4caeda2215ba63d3e39b0a41057cd6c1410bd
Noah-Su <sjh5353132@126.com>
2015-05-22 16:59:37 +0800

    Update Card.h

--- Card.h
+++ Card.h
@@ -1,4 +1,5 @@
-
+#ifndef zcx_1
+#define zcx_1
 struct Card
 {
 	int color, val;
@@ -10,7 +11,7 @@ struct Card
 
 bool operator <(const Card &rx1, const Card &rx2);
 
-struct Card7
+struct Card7 //将牌组赋值给card7前必须排序 
 {
 	Card card[7];
 	int level; // range: 1 ~ 9 
@@ -26,3 +27,5 @@ struct Card7
 
 bool operator <(const Card7 &rx1, const Card7 &rx2);
 void sort_card7(Card7 card_array[], int N);
+
+#endif
