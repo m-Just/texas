8556543e68a30834a804bd34fcc57d8ba62474ca
m-Just <mjust.lkc@gmail.com>
2015-05-22 16:50:04 +0800

    Update card_level.cpp

--- card_level.cpp
+++ card_level.cpp
@@ -2,17 +2,8 @@
 #include<cstdlib>
 #include<algorithm>
 #include<iostream>
+#include"constant.h"
 using namespace std;
-
-#define HIGH_CARD 1
-#define ONE_PAIR  2
-#define TWO_PAIR  3
-#define THREE_OF_A_KIND 4
-#define STRAIGHT  5
-#define FLUSH     6
-#define FULL_HOUSE 7
-#define FOUR_OF_A_KIND 8
-#define STRAIGHT_FLUSH 9
 
 struct Card
 {
