21c00999895b80830bd59c6cbaf6d264f6f4858f
m-Just <mjust.lkc@gmail.com>
2015-05-22 16:48:47 +0800

    Create constant.h

---
+++ constant.h
@@ -0,0 +1,22 @@
+/* 
+ * This file defines all constants in common use.
+ */
+
+/* Actions */
+#define CHECK 1
+#define CALL  2
+#define RAISE 3
+#define ALLIN 4
+#define FOLD  5
+#define BLIND 6
+
+/* Poker Hands */
+#define HIGH_CARD 1
+#define ONE_PAIR  2
+#define TWO_PAIR  3
+#define THREE_OF_A_KIND 4
+#define STRAIGHT  5
+#define FLUSH     6
+#define FULL_HOUSE 7
+#define FOUR_OF_A_KIND 8
+#define STRAIGHT_FLUSH 9
