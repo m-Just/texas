c8d785266fdb054ec2de163fbfa6d021ce5dd2e4
jiangyoushizhe <1104481071@qq.com>
2015-05-22 15:41:01 +0800

    Update read and action.c

--- read and action.c
+++ read and action.c
@@ -7,6 +7,7 @@
 #define RAISE 3
 #define ALLIN 4
 #define FOLD  5
+#define BLIND 6
 
 void get_word(char **msg, int fd)
 {
@@ -64,12 +65,121 @@ void action(int x, int num, int fd)//1:check  2:call  3:raise num  4:all_in  5:f
     write(fd, action_msg, sizeof(action_msg));
 }
 
-  //action(kind, bet number, socket);
+
+	//action(kind, bet number, socket);
 	//get_word(&a_word_name, socket);
 	//change_to_num(a_word_name, &a_number_name);
 
+#include "conversion.h"
 
+struct player
+{
+	int pid, jetton, money;
+}button, sblind, bblind, nor[10];//nor[0].pid is the number of other players.
+//nor: other players.
 
+struct player_in_game
+{
+	int pid, jetton, money, bet, action; //1:check  2:call  3:raise  4:all_in  5:fold  6:blind
+}done[10]; //done[0].pid is the number of players.
+
+int hold[5], com[10];
+
+int plnum = 0; //player number
+int pot = 0;
+
+int get_msg(int fd)//1:seat_info  2:game_over  3:blind  4:hold  5:inquire  6:
+{
+	char *msg;
+	get_word(&msg, fd);
+	if(strcmp(msg, "seat/") == 0){
+		nor[0].pid = 0;
+		plnum = 0;
+		while(1){
+			get_word(&msg, fd);
+			if(strcmp(msg, "/seat") == 0)return 1;
+			if(strcmp(msg, "button:") == 0){
+				int num = 0;
+				get_word(&msg, fd); change_to_num(msg, &num); button.pid = num;	
+				get_word(&msg, fd); change_to_num(msg, &num); button.jetton = num;
+				get_word(&msg, fd); change_to_num(msg, &num); button.money = num;	
+				plnum ++;
+			}else if(strcmp(msg, "small blind:") == 0){
+				int num = 0;
+				get_word(&msg, fd); change_to_num(msg, &num); sblind.pid = num;	
+				get_word(&msg, fd); change_to_num(msg, &num); sblind.jetton = num;
+				get_word(&msg, fd); change_to_num(msg, &num); sblind.money = num;
+				plnum ++;	
+			}else if(strcmp(msg, "big blind:") == 0){
+				int num = 0;
+				get_word(&msg, fd); change_to_num(msg, &num); bblind.pid = num;	
+				get_word(&msg, fd); change_to_num(msg, &num); bblind.jetton = num;
+				get_word(&msg, fd); change_to_num(msg, &num); bblind.money = num;
+				plnum ++;	
+			}else{
+				int num = 0;
+				nor[0].pid ++;
+				int x = nor[0].pid;
+				change_to_num(msg, &num); nor[x].pid = num;	
+				get_word(&msg, fd); change_to_num(msg, &num); nor[x].jetton = num;
+				get_word(&msg, fd); change_to_num(msg, &num); nor[x].money = num;	
+				plnum ++;
+			}
+		}	
+	}
+	if(strcmp(msg, "game-over") == 0)return 2;
+	if(strcmp(msg, "blind/") == 0){
+		int num = 0;
+		get_word(&msg, fd); get_word(&msg, fd); change_to_num(msg, &num); sblind.jetton -= num;
+		while(1){
+			get_word(&msg, fd);
+			if(strcmp(msg, "/blind") == 0)return 3;
+			else{
+				get_word(&msg, fd); change_to_num(msg, &num); bblind.jetton -= num;
+			}
+		}
+	}
+	if(strcmp(msg, "hold/") == 0){
+		char *col;
+		char *poi;
+		while(1){
+			get_word(&msg, fd);
+			if(strcmp(msg, "/hold") == 0)return 4;
+			else{
+				strcpy(col, msg);
+				get_word(&msg, fd); strcpy(poi, msg);
+				hold[0]++;
+				hold[hold[0]] = ctoi(col, poi);
+			}
+		}	
+	}
+	if(strcmp(msg, "inquire/") == 0){
+		while(1){
+			int num = 0;
+			get_word(&msg, fd);
+			if(strcmp(msg, "/inquire") == 0)return 5;
+			else if(strcmp(msg, "total") == 0){
+				get_word(&msg, fd); get_word(&msg, fd);
+				change_to_num(msg, &num);
+				pot = num;
+			}else{
+				done[0].pid++;
+				int x = done[0].pid;
+				change_to_num(msg, &num); done[x].pid = num;
+				get_word(&msg, fd); change_to_num(msg, &num); done[x].jetton = num;
+				get_word(&msg, fd); change_to_num(msg, &num); done[x].money = num;
+				get_word(&msg, fd); change_to_num(msg, &num); done[x].bet = num;
+				get_word(&msg, fd);
+				if(strcmp(msg, "ckeck") == 0)done[x].action = 1;
+				if(strcmp(msg, "call") == 0)done[x].action = 2;
+				if(strcmp(msg, "raise") == 0)done[x].action = 3;
+				if(strcmp(msg, "all_in") == 0)done[x].action = 4;
+				if(strcmp(msg, "fold") == 0)done[x].action = 5;
+				if(strcmp(msg, "blind") == 0)done[x].action = 6;
+			}
+		}
+	}
+}
 
 
 
