8e8b299d2913bffb8f14f055479cf47c48bed43d
Noah-Su <sjh5353132@126.com>
2015-05-22 16:57:30 +0800

    Create Card.h

---
+++ Card.h
@@ -0,0 +1,28 @@
+
+struct Card
+{
+	int color, val;
+	Card(void)
+	{
+		color = val = -1;
+	}
+};
+
+bool operator <(const Card &rx1, const Card &rx2);
+
+struct Card7
+{
+	Card card[7];
+	int level; // range: 1 ~ 9 
+	int level2; 
+	
+	Card7(void)
+	{
+		level = -1; 
+	} 
+	
+	int get_level(void);
+};
+
+bool operator <(const Card7 &rx1, const Card7 &rx2);
+void sort_card7(Card7 card_array[], int N);
