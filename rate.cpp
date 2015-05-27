6ffdb11ba7761e6e301e3f57e1aad618f73514d7
Noah-Su <sjh5353132@126.com>
2015-05-27 23:23:58 +0800

    。。

--- rate.cpp
+++ rate.cpp
@@ -3,6 +3,8 @@
 #include<algorithm>
 #include<cstdlib>
 #include"Card.h"
+#include<string.h>
+#include"constant.h"
 using namespace std;
 
 rate make_pair(double x1, double x2)
@@ -20,10 +22,15 @@ rate operator +(const rate &rx1, const rate &rx2)
 
 rate dfs(bool flag[][14], const Card hand_card[], Card public_card[], const int &public_card_number, const int &player_number)
 {
+	if (public_card_number > 5)
+	{
+		printf("warning! public_card_number too large!\n");
+		exit(0);
+	}
 	if (public_card_number == 5)
 	{
 		Card7 c7;
-		Card7 c7_in_order[1300];
+		static Card7 c7_in_order[3000];
 		int cnt = 0;
 
 		for (int i = 0; i < 5; i++) c7.card[i] = public_card[i];
@@ -43,17 +50,20 @@ rate dfs(bool flag[][14], const Card hand_card[], Card public_card[], const int 
 		c7.get_level();
 		int left_boundary, right_boundary;
 		left_boundary = 0;
-		while (!(c7_in_order[left_boundary].level == c7.level && c7_in_order[left_boundary].level2 == c7.level2)) left_boundary++;
+		while (left_boundary < cnt && c7_in_order[left_boundary].level < c7.level) left_boundary++;
+		while (left_boundary < cnt && c7_in_order[left_boundary].level == c7.level && c7_in_order[left_boundary].level2 < c7.level2) left_boundary++;
 		right_boundary = left_boundary;
-		while (c7_in_order[right_boundary].level == c7.level && c7_in_order[right_boundary].level2 == c7.level2) right_boundary++;
+		while (right_boundary < cnt && c7_in_order[right_boundary].level == c7.level && c7_in_order[right_boundary].level2 == c7.level2) right_boundary++;
 		double win_rate = 1, draw_rate = 1;
 		for (int i = 0; i < player_number - 1; i++) win_rate *= (double)left_boundary / cnt, draw_rate *= (double)right_boundary / cnt;
 		draw_rate = draw_rate - win_rate;
 		return make_pair(draw_rate, win_rate);
-		//return make_pair((double)left_boundary / cnt, (double)(right_boundary - left_boundary) / cnt);
 	}
 	else
 	{
+#ifdef _TEST
+		print_Card(public_card, public_card_number, "public card when calculating rate");
+#endif
 		int cnt = 0;
 		rate res = make_pair((double)0, (double)0);
 		for (int i = 1; i <= 4; i++)
@@ -73,7 +83,11 @@ rate dfs(bool flag[][14], const Card hand_card[], Card public_card[], const int 
 
 rate win_rate(const Card hand_card[], Card public_card[], const int &public_card_number, const int &player_number)
 {
-	bool flag[5][14] = { 0 };
+#ifdef _TEST
+	print_Card(public_card, public_card_number, "public for win rate");
+#endif
+	static bool flag[5][14];
+	memset(flag, 0, sizeof(flag));
 	for (int i = 0; i < 2; i++) flag[hand_card[i].color][hand_card[i].val] = 1;
 	for (int i = 0; i < public_card_number; i++) flag[public_card[i].color][public_card[i].val] = 1;
 
