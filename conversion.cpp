1cabfd17a6c5468019158879c45dcb9a4542d119
Noah-Su <sjh5353132@126.com>
2015-05-27 10:55:14 +0800

    Create conversion.cpp

---
+++ conversion.cpp
@@ -0,0 +1,47 @@
+#include"conversion.h"
+#include"Card.h"
+#include<stdlib.h>
+
+int ctoi(char* color, char* point) {      // char* requires the header of the string
+	int c, p;
+	if (point[0] > 64) {
+		switch ((int)point[0]) {
+			case 65: p = 1;  break;
+			case 74: p = 11; break;
+			case 81: p = 12; break;
+			case 75: p = 13; break;
+		}
+	} else 	p = atoi(point);
+
+	switch(color[0]) {
+		case 'H': c = 0; break;
+		case 'D': c = 1; break;
+		case 'S': c = 2; break;
+		case 'C': c = 3; break;
+	}
+
+	return (p-1)*4 + c;
+}
+
+
+int colorof(int n) {
+	return n % 4;
+}
+
+int pointof(int n) {
+	return n / 4 + 1;
+}
+
+//convert read to win_rate
+void change_to_Card(Card pubc[], Card handc[], int *hold, int *com)
+{
+	int i;
+	for(i = 1; i <= hold[0]; i++){
+		handc[i].color = colorof(hold[i]);
+		handc[i].val = pointof(hold[i]);
+	}
+	for(i = 1; i <= com[0]; i++){
+		pubc[i].color = colorof(com[i]);
+		pubc[i].val = pointof(com[i]);
+	}
+}
