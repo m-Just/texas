5eb544fe0a59922ca2493c3ffd8d84d4a21d0957
Noah-Su <sjh5353132@126.com>
2015-05-28 13:37:42 +0800

    move

--- conversion.cpp
+++ conversion.cpp
@@ -1,6 +1,41 @@
 #include "conversion.h"
 #include "Card.h"
+#include<string.h>
 #include <stdlib.h>
+
+char * strrev(char *str)
+{
+	char *right = str;
+	char *left = str;
+	char ch;
+	while (*right)   right++;
+	right--;
+	while (left < right)
+	{
+		ch = *left;
+		*left++ = *right;
+		*right-- = ch;
+	}
+	return(str);
+}
+
+int change_to_num(char msg[], int *number)// return -1: not number    return 0: is number
+{
+	int l = strlen(msg), flag = 1;
+	char tmp[l + 5];
+	strcpy(tmp, msg);
+	int num = 0, i;
+	for (i = 0; i < l; i++){
+		if (i == 0 && tmp[i] == '-')flag = -1;
+		else if (tmp[i] >= '0' && tmp[i] <= '9'){
+			num *= 10;
+			num += tmp[i] - 48;
+		}
+		else if (i != l - 1 || tmp[i] != ':')return -1;
+	}
+	*number = num * flag;
+	return 0;
+}
 
 int ctoi(char* color, char* point) {      // char* requires the header of the string
 	int c, p;
